d01df8489571276267c6c0aaa76af2c6e033e3af
Alexandre Moreira <4lexandre.moreira@gmail.com>
2024-04-26 15:10:38 -0300

    feat: controle de fluxo

---
+++ controle-de-fluxo/src/Contador.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Contador {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("======Controle de Fluxo============\n");
+        System.out.println("Digite o primeiro parâmetro");
+        int parametroUm = scanner.nextInt();
+
+        System.out.println("Digite o segundo parâmetro");
+        int parametroDois = scanner.nextInt();
+
+        try{
+           contar(parametroUm, parametroDois);
+        }catch(ParametrosInvalidosException e){
+            System.out.println("O segundo valor deve ser maior que o primeiro");
+        }
+    }
+    static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException{
+        if(parametroUm >= parametroDois){
+            throw new ParametrosInvalidosException("O segundo valor deve ser maior que o primeiro");
+        }
+
+        int contagem = parametroDois - parametroUm;
+        System.out.println("\n============Resultado==============");
+        for(int i = 0; i<= contagem; i++){
+            System.out.println(parametroUm + i);
+        }
+        
+    }
+}
